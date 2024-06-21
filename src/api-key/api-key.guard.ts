62ea38bddbe875a16a7ccb7bec874a8c98cf22e6
Florian Wolf <florian.wolf5@outlook.com>
2024-06-21 21:56:22 +0200

    switch key method

--- src/api-key/api-key.guard.ts
+++ src/api-key/api-key.guard.ts
@@ -8,7 +8,7 @@ export class ApiKeyGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const apiKey = request.headers['x-api-key'];
+    const apiKey = request.query.key;
     const validApiKey = this.configService.get<string>('API_KEY');
 
     if (apiKey && apiKey === validApiKey) {
