903e78b84ef6b8d3978c797876fda37626571480
Florian Wolf <florian.wolf5@outlook.com>
2024-06-21 22:49:02 +0200

    change ics format

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,75 +1,126 @@
-import {Injectable, Logger} from '@nestjs/common';
-import {createEvent, EventAttributes} from "ics";
+import { Injectable, Logger } from '@nestjs/common';
+import { EventAttributes } from 'ics';
 import * as schedule from 'node-schedule';
-import * as fs from "fs";
+import * as fs from 'fs';
 
 @Injectable()
 export class ApiService {
-    private readonly logger = new Logger(ApiService.name);
-    private data: EventAttributes[] = [];
+  private readonly logger = new Logger(ApiService.name);
+  private data: EventAttributes[] = [];
 
-    constructor() {
-        this.loadData();
-        // Schedule the fetchData method to run every 10 minutes
-        schedule.scheduleJob('* * * * *', () => this.loadData());
+  constructor() {
+    this.loadData();
+    // Schedule the fetchData method to run every 10 minutes
+    schedule.scheduleJob('* * * * *', () => this.loadData());
+  }
+
+  async loadData() {
+    try {
+      this.logger.log('Loading data');
+      this.data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+      this.data = this.transformToICSFormat(this.data);
+    } catch (error) {
+      this.logger.error('Error loading data', error);
+    }
+  }
+
+  transformToICSFormat(data: any): EventAttributes[] {
+    return data.map(item => {
+      // Konvertiere Unix-Zeitstempel in Datum und Uhrzeit
+      const startDate = new Date(item.start * 1000);
+      const endDate = new Date(item.end * 1000);
+
+      // Extrahiere Datum und Uhrzeitkomponenten
+      const start = [
+        startDate.getFullYear(),
+        startDate.getMonth() + 1, // Monate sind 0-basiert in JavaScript
+        startDate.getDate(),
+        startDate.getHours(),
+        startDate.getMinutes(),
+      ];
+
+      const end = [
+        endDate.getFullYear(),
+        endDate.getMonth() + 1,
+        endDate.getDate(),
+        endDate.getHours(),
+        endDate.getMinutes(),
+      ];
+
+      return {
+        title: item.title,
+        start: start,
+        end: end,
+        description: item.description,
+        location: item.room,
+        status: 'CONFIRMED',
+        organizer: { name: item.instructor },
+      };
+    });
+  }
+
+  getICSData(): string {
+    let eventString = 'BEGIN:VCALENDAR\n' +
+      'VERSION:2.0\n' +
+      'CALSCALE:GREGORIAN\n' +
+      'METHOD:PUBLISH\n' +
+      'X-WR-TIMEZONE:Europe/Berlin\n';
+
+    for (const event of this.data) {
+      eventString += this.createICSEvent(event);
     }
 
-    async loadData() {
-        try {
-            this.logger.log('Loading data');
-            this.data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-            this.data = this.transformToICSFormat(this.data);
-        } catch (error) {
-            this.logger.error('Error loading data', error);
-        }
-    }
+    eventString += 'END:VCALENDAR';
 
-    transformToICSFormat(data: any): EventAttributes[] {
-        return data.map(item => {
-            // Konvertiere Unix-Zeitstempel in Datum und Uhrzeit
-            const startDate = new Date(item.start * 1000);
-            const endDate = new Date(item.end * 1000);
+    return eventString;
+  }
 
-            // Extrahiere Datum und Uhrzeitkomponenten
-            const start = [
-                startDate.getFullYear(),
-                startDate.getMonth() + 1, // Monate sind 0-basiert in JavaScript
-                startDate.getDate(),
-                startDate.getHours(),
-                startDate.getMinutes(),
-            ];
+  createICSEvent(event: EventAttributes): string {
+    let eventString: string = '';
 
-            const end = [
-                endDate.getFullYear(),
-                endDate.getMonth() + 1,
-                endDate.getDate(),
-                endDate.getHours(),
-                endDate.getMinutes(),
-            ];
-
-            return {
-                title: item.title,
-                start: start,
-                end: end,
-                description: item.description,
-                location: item.room,
-                status: 'CONFIRMED',
-                organizer: {name: item.instructor},
-            };
-        });
-    }
-
-    getICSData(): string {
-        let icsString = '';
-        this.data.forEach(event => {
-            createEvent(event, (error, value) => {
-                if (error) {
-                    this.logger.error('Error creating ICS event', error);
+    const convertDate = (date: Array<number>): string => {
+        let out = '';
+        if (Array.isArray(event.start)) {
+            event.start.forEach((val, index) => {
+                // if (index === 0) {
+                //     return
+                // }
+                const str_val = val.toString();
+                if (str_val.length === 1) {
+                    out += '0' + str_val;
                 } else {
-                    icsString += value + '\n';
+                    out += str_val;
                 }
-            });
-        });
-        return icsString;
+                if (index === 2) {
+                    out += 'T';
+                }
+            })
+            out += '00';
+        }
+
+        return out;
     }
+
+    // Convert start and end DateTime objects to strings
+    let start = '';
+    if (Array.isArray(event.start)) {
+        start = convertDate(event.start)
+    }
+
+    let end = '';
+    if (Array.isArray(event['end'])) {
+        end = convertDate(event['end'])
+    }
+
+    eventString += 'BEGIN:VEVENT\n';
+    eventString += `DTSTART:${start}\n`;
+    eventString += `DTEND:${end}\n`;
+    eventString += `DTSTAMP:${new Date().toISOString().replace(/[-:.]/g, '')}Z\n`;
+    eventString += `UID:${Math.random().toString(36).substring(2)}@example.com\n`;
+    eventString += `DESCRIPTION:${event.description}\n`;
+    eventString += `SUMMARY:${event.title}\n`;
+    eventString += 'END:VEVENT\n';
+
+    return eventString;
+  }
 }
